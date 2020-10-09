99f368ebbc1a63f5db9c6fd72bfc56368764a2a8
Adnan Erdoğan <thinkcodeforlife@users.noreply.github.com>
2020-10-09 12:29:05 +0300

    Change field delimiter to tabs and print tasks which belong to me

--- notes.sh
+++ notes.sh
@@ -18,8 +18,27 @@ awk 'NR==1{printf "%s",$0; gsub(/[^;]/,""); nlast=n=length($0); next;} nlast==n{
 # py csv_to_tsv.py tasks.csv -> results.csv oldu
 # *************************************
 
+
+# *************************************
+# TEST: tesing awk on tasks data
+# *************************************
 cat tasks2.csv | tr -s "\t" ","
 cat tasks2.csv | cut -d " " -f1,2,3
+
+# fields: SIRA    KONU    SORUMLU PER▒YOT M▒AT    ▒telenen Tarih  DURUM   TEBL▒▒ TAR▒H▒   A▒IKLAMA
+awk '{ print }' tasks2.data
+awk '{ print $1 }' tasks2.data
+awk '{ print $1, $3 }' tasks2.data
+awk '{ print $1, $2 }' tasks2.data
+awk '{ print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 }' tasks2.data
+
+awk '$3 == "Adnan" { print }' tasks2.data
+awk '$3 ~ /.*dnan.*/ { print }' tasks2.data
+
+awk '$5 ~ /.*dnan.*/ { print }' tasks2.data
+awk '$6 ~ /.*dnan.*/ { print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 }' tasks2.data
+# *************************************
+
 
 # *************************************
 # STUDY: Active usage of Awk
@@ -31,15 +50,5 @@ awk 'BEGIN { FS="\t" } { print }' tasks2.data
 awk 'BEGIN { FS="\t" } { print $1, $2, $3 }' tasks2.data
 awk 'BEGIN { FS="\t" } $3 ~ /.*dnan.*/ { print $1, $2, $3 }' tasks2.data
 
-awk '{ print }' tasks2.data
-awk '{ print $1 }' tasks2.data
-awk '{ print $1, $3 }' tasks2.data
-awk '{ print $1, $2 }' tasks2.data
-awk '{ print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 }' tasks2.data
-# fields: SIRA    KONU    SORUMLU PER▒YOT M▒AT    ▒telenen Tarih  DURUM   TEBL▒▒ TAR▒H▒   A▒IKLAMA
-awk '$3 == "Adnan" { print }' tasks2.data
-awk '$3 ~ /.*dnan.*/ { print }' tasks2.data
-
-awk '$5 ~ /.*dnan.*/ { print }' tasks2.data
-awk '$6 ~ /.*dnan.*/ { print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 }' tasks2.data
 # *************************************
+
